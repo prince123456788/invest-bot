261cb0664faea85329fd3daeef68981bfbfb719a
prince123456788 <108258003+prince123456788@users.noreply.github.com>
2024-09-15 09:47:25 +0100

    _setup.js

--- commands/_setup.js
+++ commands/_setup.js
@@ -9,12 +9,13 @@
   aliases: 
 CMD*/
 
-Libs.CoinPayments.setPrivateKey("e2461f3FF79d44fB54C8ECF46451663c3cEEe539d8f2E62f0DbF06dc87d093D7");
-Libs.CoinPayments.setPublicKey('0b0ae9fd536f36c5815037fe7303a2d4c1f466535900faf97b51390c67e02e5d');
+Libs.CoinPayments.setPrivateKey("7423be11e2F94d77f969D622634b8705Bdf6c071dD8cA5185315a5D8ECb432C5");
+Libs.CoinPayments.setPublicKey('4d9dd83daf46125a6754e78e7ee0ed498b49495348a2acc8ea0a5dea8437bcb7');
 
 // Get your BB Api Key from Bots.Business App in Profile
-Libs.CoinPayments.setBBApiKey('0b0ae9fd536f36c5815037fe7303a2d4c1f466535900faf97b51390c67e02e5d');Libs.CoinPayments.setPrivateKey("e2461f3FF79d44fB54C8ECF46451663c3cEEe539d8f2E62f0DbF06dc87d093D7");
-Libs.CoinPayments.setPublicKey('ce4ca2c5009c8d638eddf5263a7a39c57c3aa4d691531d852c1288c568db1e9b');
+//Libs.CoinPayments.setBBApiKey('0b0ae9fd536f36c5815037fe7303a2d4c1f466535900faf97b51390c67e02e5d');
+//Libs.CoinPayments.setPrivateKey("e2461f3FF79d44fB54C8ECF46451663c3cEEe539d8f2E62f0DbF06dc87d093D7");
+//Libs.CoinPayments.setPublicKey('ce4ca2c5009c8d638eddf5263a7a39c57c3aa4d691531d852c1288c568db1e9b');
 
 // Get your BB Api Key from Bots.Business App in Profile
-Libs.CoinPayments.setBBApiKey('Fdbsf7TbJGIKRuYbDCUs_Qn011ukMo4yag8CSfAb');
+Libs.CoinPayments.setBBApiKey('eMRbdIXdmL2hiBNe_dBXwnxqeU81YMUs3XDW8Uc6');
